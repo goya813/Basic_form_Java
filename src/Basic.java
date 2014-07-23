ecfe4a0fdb71644742634094dc0da74d55079d46
goya813 <wasabibeef@gmail.com>
2014-07-23 13:24:42 +0900

    Main関数を作った

---
+++ src/Basic.java
@@ -0,0 +1,15 @@
+
+public class Basic {
+	public static void Main(String argv[])
+	{
+		initBasic();
+		
+		output_console("JAVA BASIC ver0.0.0.1");
+		console();
+	}
+	
+	private static void initBasic()
+	{
+		
+	}
+}
